8db6e7146d8b14b53101f636efa81ab848e8d23d
80947108 <42614839+80947108@users.noreply.github.com>
2023-10-12 18:31:52 +0800

    Update index.php

--- api/index.php
+++ api/index.php
@@ -76,7 +76,7 @@
 				<button type="submit" onclick="play()" class="btn btn-primary btn-lg btn-block"> 解析播放 </button>
 				</div>		
 			    </div>					
-       		<--<script src="https://cdn.dearxuan.com/website/live2d-widget/autoload.js"></script>-->
+       		<!--<script src="https://cdn.dearxuan.com/website/live2d-widget/autoload.js"></script>-->
 <script src="https://fastly.jsdelivr.net/gh/stevenjoezhang/live2d-widget@latest/autoload.js"></script>
 			<!-- 公告 -->
 			<div class="panel panel-default">
